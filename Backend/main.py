13dac0ed606ffebc8c49429f90d16a0f687b9a7d
nishtha chaturvedi <nishthachaturvedi10@gmail.com>
2026-05-18 23:10:35 +0530

    cors fixed

--- Backend/main.py
+++ Backend/main.py
@@ -11,10 +11,12 @@ from routes.risk import router as risk_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://kizashi-sigma.vercel.app",
+        "http://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
